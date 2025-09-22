56c7522a899e791351676eb419fa5052c2e08523
nichiki <nichiki@gmail.com>
2025-09-22 14:03:06 +0900

    SearchParamsのawait問題に対応

--- app/news/search/page.tsx
+++ app/news/search/page.tsx
@@ -4,15 +4,14 @@ import NewsList from "@/app/_components/NewsList";
 import SearchField from "@/app/_components/SearchField";
 
 type Props = {
-    searchParams: {
-        q?: string;
-    }
+    searchParams: Promise<{q?: string }>;
 };
 
 export default async function Page({ searchParams }: Props) {
+    const { q } = await searchParams;
     const { contents: news } = await getNewsList({
         limit: NEWS_LIST_LIMIT,
-        q: searchParams.q,
+        q: q,
     });
 
     return (
